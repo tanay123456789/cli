720fdc47ecf05b38574ee5fbbc5b26988ee5d9df
Tanay Dwivedi <tanaydwivedi456@gmail.com>
2020-11-17 00:05:04 +0530

    Added a function

--- cli_app/index.js
+++ cli_app/index.js
@@ -15,16 +15,37 @@ var welcomeMessage=`Welcome ${player}`
 console.log(welcomeMessage);
 
 
-
+var score=0;
 var answer=rs.question(`What is my profession?`);
 
 var check;
 
 if(answer==="Student"||answer==="student"||answer==="STUDENT"){
     check="right answer";
+    score+=1;
 }
 else{
     check="wrong answer";
+    score-=1;
 }
 
 console.log(`Hey! It was a ${check}`);
+console.log(`your score is ${score}`);
+
+function gameplay(question,answer,score){
+    var isAnswerCorrect;
+
+    var answer=rs.question(question);
+
+    if(answer===correctAnswer){
+        isAnswerCorrect=true;
+    }
+
+
+    return isAnswerCorrect,updatedScore
+}
+
+
+
+
+
