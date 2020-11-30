c71cb747f2b009acece948ed35610c6273221175
Tanay Dwivedi <tanaydwivedi456@gmail.com>
2020-11-30 15:08:19 +0530

    Added comments

--- cli_app/index.js
+++ cli_app/index.js
@@ -1,15 +1,24 @@
+
+//dependency
 var rs=require("readline-sync");
+
+//starting messages
 console.log("Hello welcome to my game");
+
 var player=rs.question("Please enter your name");
+
 var welcomeMessage=`Welcome ${player}`;
 
 console.log(welcomeMessage);
 
+console.log("You will get 1 score for correct answer and -1 for wrong answer")
+
+
+//variable to record score
 var score=0;
 
 
-
-
+//Function to ask question and answer
 function gameplay(question,CorrectAnswer){
     var isAnswerCorrect;
 
@@ -26,24 +35,44 @@ function gameplay(question,CorrectAnswer){
     }
     return isAnswerCorrect;
 }
-var qna={
+
+
+//List of questions
+const questionList=[{
     question:"What is my profession?",
     answer:"student"
+},{
+    question:"Who's my favourite anime character?",
+    answer:"L"
+},{
+    question:"Where do I live?",
+    answer:"Lucknow"
+
+
+},{
+    question:"What's my favourite superhero character?",
+    answer:"IronMan"
+}];
+
+
+
+//Loop to call gameplay funtion
+for(var i=0;i<questionList.length;i++){
+    var currentQuestion=questionList[i];
+    var check=gameplay(currentQuestion.question,currentQuestion.answer);
+    if(check===true){
+
+        console.log(`Hey! It was a right answer`);
+        console.log(`your score is ${score}`);
+    }
+    
+    else{
+        console.log(`Wrong answer`);
+        console.log(`your score is ${score}`);
+    
+    }
+    
 }
-const check=gameplay('qna.question','qna.answer');
-if(check===true){
-
-    console.log(`Hey! It was a right answer`);
-    console.log(`your score is ${score}`);
-}
-
-else{
-    console.log(`Hey! It was a right answer`);
-    console.log(`your score is ${score}`);
-
-}
-
-
 
 
 
